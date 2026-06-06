dc5f420111da05933e8e72bb91d275271dc757bf
khaled-bassam <khaledabusitta2262003@gmail.com>
2026-06-06 18:51:25 +0300

    Add update and destroy methods to UserController

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,41 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
 {
-    //
+  
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'name'     => 'required|string|max:255',
+            'email'    => 'required|email|unique:users,email,' . $id,
+            'password' => 'nullable|string|min:8',
+        ]);
+
+        $user = User::findOrFail($id);
+
+        if (!empty($validatedData['password'])) {
+            $validatedData['password'] = Hash::make($validatedData['password']);
+        } else {
+            unset($validatedData['password']);
+        }
+
+      
+        $user->update($validatedData);
+
+        return redirect()->route('users.index')->with('success', 'تم تحديث بيانات المستخدم بنجاح!');
+    }
+
+   
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return redirect()->route('users.index')->with('success', 'تم حذف المستخدم بنجاح!');
+    }
 }
